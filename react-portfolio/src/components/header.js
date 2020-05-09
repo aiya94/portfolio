474cf377cc6c7ffb46927bcf88aee95834b0ba26
Aiya <48046501+aiya94@users.noreply.github.com>
2020-05-09 23:59:29 +0800

    create component folder

---
+++ react-portfolio/src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CreateHeader() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My new portfolio</p>
+      </header>
+    </div>
+  );
+}
+
+export default CreateHeader;
